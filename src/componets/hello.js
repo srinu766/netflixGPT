478bc664f137abc1ff1a1e30262d85c4194fbc41
srinu <sopparisrinu637@gmail.com>
2024-08-02 07:55:31 +0530

    first

---
+++ src/componets/hello.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const hello = () => {
+  return (
+        <div className="App">
+          <div className="container">
+            <h1 className="">Hello, Bootstrap!</h1>
+            <button className="btn btn-primary">Click me!kkk</button>
+          </div>
+        </div>
+  )
+}
+
+export default hello
